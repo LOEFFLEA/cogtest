3e6f469b2cd9079c38fdd6c3801770000eb2ca18
isabellacurtorillo <159484441+isabellacurtorillo@users.noreply.github.com>
2025-02-28 15:54:25 -0600

    Add files via upload
    created home page component - need to style navbar

---
+++ project/home/src/app/home-screen/home-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-home-screen',
+  templateUrl: './home-screen.component.html',
+  styleUrls: ['./home-screen.component.scss']
+})
+export class HomeScreenComponent {
+
+}
